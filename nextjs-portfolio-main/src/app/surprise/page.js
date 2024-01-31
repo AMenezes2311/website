297a0ccd1b52f6a207ff55654bf94eb5d7d9fcc9
André Menezes <andremenezes231@hotmail.com>
2024-01-31 12:39:13 -0500

    Update page.js

--- nextjs-portfolio-main/src/app/surprise/page.js
+++ nextjs-portfolio-main/src/app/surprise/page.js
@@ -1,14 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
- 
-export default function Document() {
+import HeroSection from "../components/HeroSection";
+import Navbar from "../components/Navbar";
+import AboutSection from "../components/AboutSection";
+import ProjectsSection from "../components/ProjectsSection";
+import Footer from "../components/Footer";
+import ConnectSection from "../components/ConnectSection"
+
+import React from 'react';
+
+const UnderConstruction = () => {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <h1>TEST</h1>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+    <div class="container">
+      <div class="valentines">
+        <div class="envelope"></div>
+        <div class="front"></div>
+        <div class="card">
+          <div class="text">Will<br> You be</br> My Valentine? </div>
+          <div class="heart"></div>
+        </div>
+        <div>
+        </div>
+        <div class="shadow"></div>
+      </div>
+      <script src="script.js"></script>
+    </div>
+  );
+};
+
+
+export default UnderConstruction;
