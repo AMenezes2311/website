c170fd4a64758d66ad3ffd2f9d6b2a478a97e1ea
André Menezes <andremenezes231@hotmail.com>
2024-02-01 16:08:46 -0500

    Update ProjectsSection.jsx

--- nextjs-portfolio-main/src/app/components/ProjectsSection.jsx
+++ nextjs-portfolio-main/src/app/components/ProjectsSection.jsx
@@ -50,7 +50,7 @@ const ProjectsSection = () => {
   return (
     <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-        My Projects<br>*Section under construction</br>
+        My Projects<br />*Section under construction
       </h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         {/*
