54d6da322b5c0669b7e3dcf6081f14ff13172242
Andre Menezes <andremenezes231@hotmail.com>
2026-01-10 17:07:48 -0500

    Adding paths to github and preview

--- nextjs-portfolio-main/src/app/components/ProjectCard.jsx
+++ nextjs-portfolio-main/src/app/components/ProjectCard.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
-const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl, backgroundSize, backgroundPosition }) => {
+const ProjectCard = ({
+  imgUrl,
+  title,
+  description,
+  gitUrl,
+  previewUrl,
+  backgroundSize,
+  backgroundPosition,
+}) => {
+  const hasGit = !!gitUrl && gitUrl !== "/";
+  const hasPreview = !!previewUrl && previewUrl !== "/";
+  const hasActions = hasGit || hasPreview;
   return (
     <div>
       <div
@@ -14,20 +25,30 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl, backgroun
           backgroundPosition: backgroundPosition || "center center",
         }}
       >
-        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
-          <Link
-            href={gitUrl}
-            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-          >
-            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-          </Link>
-          <Link
-            href={previewUrl}
-            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-          >
-            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-          </Link>
-        </div>
+        {hasActions && (
+          <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+            {hasGit && (
+              <Link
+                href={gitUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+              >
+                <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+              </Link>
+            )}
+            {hasPreview && (
+              <Link
+                href={previewUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+              >
+                <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+              </Link>
+            )}
+          </div>
+        )}
       </div>
       <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
